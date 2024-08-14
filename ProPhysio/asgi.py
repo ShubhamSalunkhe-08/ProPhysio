e375da3d389f735d2f094e49b095b87fd04a4931
Shubham Salunkhe <shubhamsalunkhe066@gmail.com>
2024-08-14 12:08:41 +0530

    first commit

---
+++ ProPhysio/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ProPhysio project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProPhysio.settings')
+
+application = get_asgi_application()
